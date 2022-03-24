373ca8ed3e64a0e164f9ef80a07f0b860eca8c74
JosephineHN <Josephine.hnielsen@hotmail.com>
2022-03-24 10:30:35 +0100

    rettelser på om os

---
+++ main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/*
+let index = 1;
+
+let pages = Array.from(document.getElementById("SoMe"));
+
+slides[index - 1].style.display = "block";
+
+const carousel = (e) => {
+    let posX = e.clientX; 
+    let viewportCenter = window.innerWidth / 2; 
+    pages.forEach(page => page.style.display = "none"); 
+    if (posX > viewportCenter) {
+        index++; 
+        if (index > pages.length) {
+            index = 1;
+    }
+} else {
+    index--; 
+    if (index < 1) {
+        index = pages.length;
+    }
+} 
+
+    pages[index - 1].style.display = "block";
+} 
+
+document.addEventListener("click", carousel);
+*/
